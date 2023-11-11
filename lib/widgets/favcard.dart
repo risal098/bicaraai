0b04418c1c8056f97b59bedb5fc7a7ddf03eb4d2
ScaraMocchie <talitarahmi211@gmail.com>
2023-11-11 14:21:30 +0700

    Update favcard.dart
    add inkwell to favcard

--- lib/widgets/favcard.dart
+++ lib/widgets/favcard.dart
@@ -4,24 +4,28 @@ import 'package:flutter/material.dart';
     return Card(
       color: Colors.transparent,
       shadowColor: Colors.transparent,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10), // Image border
-            child: SizedBox.fromSize(
-              size: const Size.fromRadius(50), // Image radius
-              child: Image.network(
-                  imageLink,
-                  fit: BoxFit.cover),
+      // add inkwell
+      child: InkWell(
+        onTap: (){},
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10), // Image border
+              child: SizedBox.fromSize(
+                size: const Size.fromRadius(50), // Image radius
+                child: Image.network(
+                    imageLink,
+                    fit: BoxFit.cover),
+              ),
             ),
-          ),
-          Text(
-            song,
-            style: const TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Text(singer),
-        ],
+            Text(
+              song,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(singer),
+          ],
+        ),
       ),
     );
   }
