9d121cbc5af9a519893ee2eb19238807f489fd34
Falah Mandira Irawan <falah_1313622038@mhs.unj.ac.id>
2023-11-19 15:47:15 +0700

    create main for login and register

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
-import "./screens/homePage.dart";
 import 'package:get/get.dart';
-//import './screens/homePage.dart';
-void main() {
+import 'package:login_register_toba/screens/auth_page.dart';
+import 'package:login_register_toba/screens/login_page.dart';
+// import 'package:login_register_toba/screens/login_page.dart';
+import 'package:login_register_toba/screens/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       // colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 255, 255)),
-        useMaterial3: true,
-      ),
-      home:HomePage() ,
+      initialRoute: '/regis',
+      getPages: [
+        GetPage(name: '/regis', page: () => const RegisterPage()),
+        GetPage(
+            name: '/login',
+            page: () => const LoginPage(),
+            transition: Transition.noTransition),
+      ],
+      debugShowCheckedModeBanner: false,
     );
   }
 }
