6868b6c56515596e2b89932b206920499b529327
ScaraMocchie <talitarahmi211@gmail.com>
2023-11-25 23:03:15 +0700

    Create splash_screen.dart
    add splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class SplashSCreen extends StatefulWidget {
+  const SplashSCreen({super.key});
+
+  @override
+  State<SplashSCreen> createState() => _SplashSCreenState();
+}
+
+class _SplashSCreenState extends State<SplashSCreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    var height = size.height;
+    var width = size.width;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        alignment: Alignment.bottomCenter,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("asset/images/logomark.png", width: 100,),
+            SizedBox(height: 10,),
+            Text("TOBA APP", style: TextStyle(color: const Color(0xff528DE7), fontSize: 30,
+            fontWeight: FontWeight.bold
+            ),)
+          ],
+
+        ),
+      ),
+    );
+  }
+}
